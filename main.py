8423e225c65811b1f5bccd338511561c23bd2ccc
Vados12 <mr.crocodile66@gmail.com>
2026-02-23 01:21:23 +0300

    add new matrix wich value =+1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import rand
 
 random_mark = np.random.randint(1, 6, size=(4, 5))
 
@@ -9,4 +10,8 @@ print("Ср. значение по столбцам:\n", np.mean(random_mark, ax
 print("Ср. значение по строкам:\n", np.mean(random_mark, axis=1))
 print("Максимальная:\n", np.max(random_mark))
 print("Минимальная:\n", np.min(random_mark))
+
+random_mark = random_mark + 1
+random_mark = np.clip(random_mark, 2, 5)
+print("Измененная матрица оценок:\n",random_mark)
 
