e6d7137cee195f4311c93546c825fa71a0fe35c4
Vados12 <mr.crocodile66@gmail.com>
2026-02-24 18:30:42 +0300

    adding describle DataFrame, adding mean_value in the subject columns

--- pandas/pandasnumpy.py
+++ pandas/pandasnumpy.py
@@ -1,4 +1,5 @@
 import array
+from pydoc import describe
 
 import pandas as pd
 import numpy as np
@@ -9,7 +10,7 @@ names = [
 ]
 
 math = np.random.randint(2, 6, size=10)
-physics = np.random.randint(2, 6, size=10)
+physic = np.random.randint(2, 6, size=10)
 english = np.random.randint(2, 6, size=10)
 age = np.random.randint(15, 19, size=10)
 attendance = np.random.randint(20, 101, size=10)
@@ -19,12 +20,38 @@ df = pd.DataFrame (
     {
         "Names": names,
         "Math": math,
-        "Physics": physics,
+        "Physic": physic,
         "English": english,
         "Age": age,
         "Attendance %": attendance
     })
 print(df)
+
+print("-------------------------------------------------------")
+
+start = df.head()
+print(start)
+
+print("-------------------------------------------------------")
+
+describe = df.describe()
+print(describe)
+
+print("-------------------------------------------------------")
+
+mean_value_math = df["Math"].mean()
+mean_value_phycic = df["Physic"].mean()
+mean_value_english = df["English"].mean()
+
+df_mean = pd.DataFrame(
+    {
+        "Math_mean_value": [mean_value_math],
+        "Phycic_mean_value": [mean_value_phycic],
+        "English_mean_value": [mean_value_english],
+    })
+print(df_mean)
+    
+
     
 
 
